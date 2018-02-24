da6ec8b6cfadf82dc234ba826e4b4b6ddaf360b4
Diederik Krols <diederik_krols@hotmail.com>
2018-02-24 21:31:11 +0100

    Initial release.

---
+++ XamlBrewer.Uwp.ExtendedSplashScreenWithLottie/ExtendedSplash.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace XamlBrewer.Uwp.ExtendedSplashScreenWithLottie
+{
+    /// <summary>
+    /// Extended Splash Screen: keeps the splash screen image in place, while doing some stuff.
+    /// </summary>
+    partial class ExtendedSplash : Page
+    {
+        internal Rect splashImageRect; // Rect to store splash screen image coordinates.
+        private SplashScreen splash; // Variable to hold the splash screen object.
+        internal bool dismissed = false; // Variable to track splash screen dismissal status.
+        internal Frame rootFrame;
+
+        public ExtendedSplash(SplashScreen splashscreen, bool loadState)
+        {
+            InitializeComponent();
+
+            // Listen for window resize events to reposition the extended splash screen image accordingly.
+            // This is important to ensure that the extended splash screen is formatted properly in response to snapping, unsnapping, rotation, etc...
+            Window.Current.SizeChanged += new WindowSizeChangedEventHandler(ExtendedSplash_OnResize);
+
+            splash = splashscreen;
+
+            if (splash != null)
+            {
+                // Register an event handler to be executed when the splash screen has been dismissed.
+                splash.Dismissed += new TypedEventHandler<SplashScreen, Object>(DismissedEventHandler);
+
+                // Retrieve the window coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+
+                // Adjust the speed of the Lottie animation.
+                SplashScreenAnimation.Speed = 3;
+            }
+
+            // Create a Frame to act as the navigation context
+            rootFrame = new Frame();
+        }
+
+        // Position the extended splash screen image in the same location as the system splash screen image.
+        private void PositionImage()
+        {
+            extendedSplashImage.SetValue(Canvas.LeftProperty, splashImageRect.X);
+            extendedSplashImage.SetValue(Canvas.TopProperty, splashImageRect.Y);
+            extendedSplashImage.Height = splashImageRect.Height;
+            extendedSplashImage.Width = splashImageRect.Width;
+
+            // Position the Lottie animation.
+            var factor = splashImageRect.Width / 620;
+            SplashScreenAnimation.SetValue(Canvas.LeftProperty, splashImageRect.X + 100 * factor);
+            SplashScreenAnimation.SetValue(Canvas.TopProperty, splashImageRect.Y + 150 * factor);
+            SplashScreenAnimation.Width = 180 * factor;
+        }
+
+        private void ExtendedSplash_OnResize(Object sender, WindowSizeChangedEventArgs e)
+        {
+            // Safely update the extended splash screen image coordinates. This function will be fired in response to snapping, unsnapping, rotation, etc...
+            if (splash != null)
+            {
+                // Update the coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+            }
+        }
+
+        // Include code to be executed when the system has transitioned from the splash screen to the extended splash screen (application's first view).
+        private async void DismissedEventHandler(SplashScreen sender, object e)
+        {
+            dismissed = true;
+
+            // Complete app setup operations here...
+            await Task.Delay(10000);
+
+            // Work done. Close the page.
+            await DismissExtendedSplash();
+        }
+
+        private async Task DismissExtendedSplash()
+        {
+            // Navigate to the main page.
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                rootFrame = new Frame();
+                rootFrame.Content = new MainPage();
+                Window.Current.Content = rootFrame;
+            });
+        }
+    }
+}
